5f1d7686ddac6d2da67ae99db7d0d2837fcc68cc
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-03-13 17:20:54 -0700

    first query works!

--- queries.sql
+++ queries.sql
@@ -1,1 +1,5 @@
-hello
+-- find the meeting recordings that took place before '2022-01-17'
+SELECT users.user_id, users.firstname,meeting.meeting_id, meeting.title, meeting.meeting_time, meetingrecording.start_time, meetingrecording.recording_number
+FROM (meetingrecording INNER JOIN meeting ON meetingrecording.meeting_id = meeting.meeting_id) INNER JOIN users ON meeting.instructor_id = users.user_id
+where meetingrecording.start_time <= '2022-01-17'
+ORDER BY users.user_id, meeting.meeting_id, meetingrecording.recording_number
