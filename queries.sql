265104b3c06e535140a835e11fe03535af22f061
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-03-13 18:36:03 -0700

    #2 done

--- queries.sql
+++ queries.sql
@@ -6,6 +6,8 @@ ORDER BY users.user_id, meeting.meeting_id, meetingrecording.recording_number
 
 
 --find the instructors who are teaching courses with 4 or more students enrolled
-SELECT course_id, COUNT(course_id) as numStudents
-FROM enrolledin
-GROUP BY course_id
+SELECT users.firstname, users.lastname, enrolledin.course_id, course.title, COUNT(enrolledin.course_id) as count
+FROM enrolledin INNER JOIN course ON enrolledin.course_id = course.course_id INNER JOIN teaches ON enrolledin.course_id = teaches.course_id INNER JOIN users ON teaches.instructor_id = users.user_id
+GROUP BY enrolledin.course_id, course.title, teaches.instructor_id, users.firstname, users.lastname
+HAVING COUNT(enrolledin.course_id) > 3
+ORDER BY enrolledin.course_id
