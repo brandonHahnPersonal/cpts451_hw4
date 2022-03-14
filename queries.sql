e7a98eeddf90f7b1c50568783249b3083fc957e8
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-03-13 23:52:21 -0700

    added semicolon

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 SELECT users.user_id, users.firstname,meeting.meeting_id, meeting.title, meeting.meeting_time, meetingrecording.start_time, meetingrecording.recording_number
 FROM (meetingrecording INNER JOIN meeting ON meetingrecording.meeting_id = meeting.meeting_id) INNER JOIN users ON meeting.instructor_id = users.user_id
 where meetingrecording.start_time <= '2022-01-17'
-ORDER BY users.user_id, meeting.meeting_id, meetingrecording.recording_number
+ORDER BY users.user_id, meeting.meeting_id, meetingrecording.recording_number;
 
 
 --find the instructors who are teaching courses with 4 or more students enrolled
@@ -10,19 +10,19 @@ SELECT users.firstname, users.lastname, enrolledin.course_id, course.title, COUN
 FROM enrolledin INNER JOIN course ON enrolledin.course_id = course.course_id INNER JOIN teaches ON enrolledin.course_id = teaches.course_id INNER JOIN users ON teaches.instructor_id = users.user_id
 GROUP BY enrolledin.course_id, course.title, teaches.instructor_id, users.firstname, users.lastname
 HAVING COUNT(enrolledin.course_id) > 3
-ORDER BY enrolledin.course_id
+ORDER BY enrolledin.course_id;
 
 
 -- find the pair of messages that are posted at the same time by different users	
 SELECT table1.meeting_id, table1.message_time, concat(table1.message_text, CHR(13), table2.message_text) AS message, table1.user_id, table2.user_id
 FROM message table1 INNER JOIN message table2 ON table1.message_time = table2.message_time
-WHERE table1.message_time = table2.message_time AND table1.user_id < table2.user_id 
+WHERE table1.message_time = table2.message_time AND table1.user_id < table2.user_id ;
 
 
 -- find the distinct instructors that teach "Machine Learning" and "Artificial Intelligence" And teach more than two courses each
 SELECT users.firstname, users.lastname
 FROM (academicfields table1 INNER JOIN academicfields table2 ON table1.instructor_id = table2.instructor_id) INNER JOIN users ON table1. instructor_id = user_id
-WHERE table1.instructor_id = table2.instructor_id AND table1.academicfield = 'Machine Learning' AND table2.academicfield = 'Artificial Intelligence'
+WHERE table1.instructor_id = table2.instructor_id AND table1.academicfield = 'Machine Learning' AND table2.academicfield = 'Artificial Intelligence';
 
 
 --Find the meetings whose durations are longer than the average duration of all meetings
@@ -33,7 +33,7 @@ HAVING duration > ALL
 	(SELECT AVG(duration)
 	 FROM meeting
 	)
-ORDER BY meeting_id
+ORDER BY meeting_id;
 
 
 --Find the meetings whose durations are longer than the average duration of the meetings for the course they belong to
@@ -44,7 +44,7 @@ WHERE table1.duration >
 	 FROM meeting averageTable
 	 WHERE table1.course_id = averageTable.course_id
 	)
-ORDER BY meeting_id
+ORDER BY meeting_id;
 
 
 
